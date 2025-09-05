2a1129274527deadceb254e54c90a8c46567b63a
Alexandre Vieira <avieira@bvp.com>
2025-09-05 19:33:01 -0300

    Bump version to 2.1.2
    - Update version in setup.py
    - Fix PyPI badges in README
    - Ready for PyPI deployment

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name="xandai-cli",
-    version="2.1.0",
+    version="2.1.2",
     author="XandAI-project",
     description="CLI Assistant with Ollama Integration and Context-Aware Interactions",
     long_description=long_description,
